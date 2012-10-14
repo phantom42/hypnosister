6c593773bfdc21fdc6f34b446737cb5a1bfdb4bf
Joe Coleman <phantom42@gmail.com>
2012-10-14 00:12:18 -0400

    Initial Commit

---
+++ hypnosister/hypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  hypnosisterAppDelegate.h
+//  hypnosister
+//
+//  Created by Joe Coleman on 10/14/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hypnosister
+//
+//  Created by Joe Coleman on 10/14/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "hypnosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([hypnosisterAppDelegate class]));
+    }
+}
